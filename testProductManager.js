f0dd9fac66db72682cdceb2023335e173e7cfc3e
gonzaloferreyra2020 <86134463+gonzaloferreyra2020@users.noreply.github.com>
2023-06-29 23:45:20 -0300

    Create testProductManager.js

---
+++ testProductManager.js
@@ -0,0 +1,32 @@
+const ProductManager = require('./ProductManager.js');
+
+const manager = new ProductManager('products.json');
+
+console.log("Obtener los productos: ",manager.getProducts());
+
+const product = {
+    title: 'televisor',
+    description: 'electrodomestico',
+    price: 75000,
+    thumbnail: 'imagen.jpg',
+    code: 'tv2023',
+    stock: 10,
+};
+manager.addProduct(product);
+
+// Obtener los productos 
+console.log("Productos cargados: ",manager.getProducts());
+
+// Obtener por id
+const productId = 1;
+const foundProduct = manager.getProductById(productId);
+console.log("Producto por ID: ",foundProduct);
+
+// Actualizar precio
+const updatedProduct = { price: 95000 };
+manager.updateProduct(productId, updatedProduct);
+console.log("NUEVO PRECIO $: ",product.price)
+
+// Eliminar producto
+manager.deleteProduct(productId);
+console.log("Productos eliminados: ",manager.getProducts());
