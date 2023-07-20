f4e752dbe7d10952ae5704bd599ecc6194361c08
gonzaloferreyra2020 <86134463+gonzaloferreyra2020@users.noreply.github.com>
2023-07-20 00:26:45 -0300

    Update views.routes.js

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,15 +1,10 @@
 import { Router } from "express";
 
-
 const router = Router();
-
 
 //routes
 router.get("/", (req,res)=>{
-    
-    //indicar la vista
     res.render("home");
 });
-
 
 export {router as viewsRouter};
