889800ca20a78af50c42829d7386b867b5a5a176
gonzaloferreyra2020 <86134463+gonzaloferreyra2020@users.noreply.github.com>
2023-07-20 00:09:00 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,6 +13,33 @@ app.use(express.urlencoded({extended:true}))
 //mensaje por consola
 app.listen(port,()=>console.log(`Server listening on port ${port}`));
 
+//handlebars
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname,"/views"));
+
+//websocket
+const socketServer = new Server(httpsServer);
+let messages = [];
+
 //routes
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
+
+socketServer.on("connection", async (socketConnected)=>{
+    console.log(`Nuevo usuario conectado  ${socketConnected.id}`);
+    //lista de productos
+    const listProductRealTime = await ProductManager.getProducts;
+    
+    //enviando lista
+    socketConnected.emit('listProductReal',listProductRealTime );
+  
+    //escuchando mensaje
+    socketConnected.on('addProduct', async(product)=>{
+        await ProductManager.addProduct(product);
+    })
+    //escuchando mensaje deleteProduct
+    socketConnected.on('deleteProduct', async(id)=>{
+        await ProductManager.deleteProduct(Number(id));
+    })
+});
