dd2bd773c6682d1548e043d7806b7c09302362e0
gonzaloferreyra2020 <86134463+gonzaloferreyra2020@users.noreply.github.com>
2023-08-21 23:57:15 -0300

    Add files via upload

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+export const config = {
+    server:{
+        port:8080
+    },
+    fileSystem:{
+        productsFile:"products.json",
+        cartFile:"carts.json"
+    },
+    mongo:{
+        url:"mongodb+srv://gonzalof188:coder@cluster0.dnmzzpb.mongodb.net/"
+    }
+}
