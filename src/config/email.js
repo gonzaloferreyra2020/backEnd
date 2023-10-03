97383f88f1f9ff40f43706b3a3e688593d24c45e
gonzaloferreyra2020 <86134463+gonzaloferreyra2020@users.noreply.github.com>
2023-10-02 23:43:12 -0300

    Add files via upload

---
+++ src/config/email.js
@@ -0,0 +1,13 @@
+//creamos el transporte para enviar los correos con gmail
+const transporter = nodemailer.createTransport({
+    host:"smtp.gmail.com",
+    port:587,
+    auth:{
+        user:adminEmail,
+        pass:adminPass
+    },
+    secure:false,
+    tls:{
+        rejectUnauthorized:false
+    }
+});
---
+++ src/public/js/home.js
@@ -0,0 +1,13 @@
+const socketClient = io(); //instanciando socket del lado del cliente
+
+const chatbox = document.getElementById("chatbox");
+const sendButton = document.getElementById("sendButton");
+
+sendButton.addEventListener("click", ()=>{
+    socketClient.emit("messageKey", chatbox.value);
+    chatbox.value="";
+});
+
+socketClient.on("messageHistory", (data)=>{
+    console.log(data);
+});
